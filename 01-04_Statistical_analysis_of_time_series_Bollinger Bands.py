dd5eee44d24cb700e6b44ee8cad8f0741242252d
Morz <m0rz@mail.ru>
2018-06-20 10:16:11 +0800

    Bollinger Bands

--- 01-04_Statistical_analysis_of_time_series_Bollinger Bands.py
+++ 01-04_Statistical_analysis_of_time_series_Bollinger Bands.py
@@ -45,6 +45,8 @@ def get_rolling_std(values, window):
 def get_bollinger_bands(rm, rstd):
     """Return upper and lower Bollinger Bands."""
     # TODO: Compute upper_band and lower_band
+    upper_band=rm+2*rstd
+    lower_band=rm-2*rstd
     return upper_band, lower_band
 
 def plot_selected(df, simbols, sdate, edate):
@@ -71,11 +73,12 @@ def test_run():
     rstd_SPY = get_rolling_std(df['SPY'], window=20)
 
     # 3. Compute upper and lower bands
-    upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
+   # upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
     
     # Plot raw SPY values, rolling mean and Bollinger Bands
     ax = df['SPY'].plot(title="Bollinger Bands", label='SPY')
     rm_SPY.plot(label='Rolling mean', ax=ax)
+    upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
     upper_band.plot(label='upper band', ax=ax)
     lower_band.plot(label='lower band', ax=ax)
     
